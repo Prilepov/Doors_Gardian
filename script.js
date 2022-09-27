14f0f4fb44aba1449cacde3c0e4782a3a37ef49c
DiLess <zzloba@gmail.com>
2022-09-28 00:11:17 +0300

    Done Lead screen, pop, sale, all_doors_1. No responsive, no mobile

---
+++ script.js
@@ -0,0 +1,42 @@
+/* Slick carousel */
+$('.pop').slick({
+    centerMode: false,
+    // centerPadding: '60px',
+    slidesToShow: 3,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: false,
+    // autoplaySpeed: 1000,
+    dots: false,
+    arrows: true,
+    prevArrow: "<img src='images/prev_arrow.png' class='prevar' alt='1'>",
+    nextArrow: "<img src='images/next_arrow.png' class='nextar' alt='1'>"
+});
+
+/* Sale slider */
+$('.sale_slider').slick({
+    centerMode: false,
+    // centerPadding: '60px',
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: false,
+});
+
+/* All doors slider */
+$('.all_doors_slider').slick({
+    centerMode: false,
+    // centerPadding: '60px',
+    slidesToShow: 2,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: false,
+    // autoplaySpeed: 1000,
+    dots: false,
+    arrows: true,
+    prevArrow: "<img src='images/prev_arrow.png' class='prevar' alt='1'>",
+    nextArrow: "<img src='images/next_arrow.png' class='nextar' alt='1'>"
+});
