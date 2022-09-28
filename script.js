f58c85472b32b013567accc7b9930a0324d42c85
DiLess <zzloba@gmail.com>
2022-09-28 19:38:44 +0300

    Done + all_doors_2 / + quiz / + all_decor_1 / +all_decor_2 / + products

--- script.js
+++ script.js
@@ -40,3 +40,18 @@ $('.all_doors_slider').slick({
     prevArrow: "<img src='images/prev_arrow.png' class='prevar' alt='1'>",
     nextArrow: "<img src='images/next_arrow.png' class='nextar' alt='1'>"
 });
+
+/* All decor slider */
+$('.all_decor_slider').slick({
+    centerMode: false,
+    // centerPadding: '60px',
+    slidesToShow: 3,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: false,
+    // autoplaySpeed: 1000,
+    dots: false,
+    arrows: true,
+    prevArrow: "<img src='images/prev_arrow.png' class='prevar' alt='1'>",
+    nextArrow: "<img src='images/next_arrow.png' class='nextar' alt='1'>"
+});
